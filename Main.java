e08095941dbf39e23f3e7201e666e84c9788ff90
Vadim <mobihelp911@gmail.com>
2016-10-08 11:56:02 +0400

    fourth commit

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main{
 	public static void main(String[] args){
-		System.out.println("d");
-		for(int i=0; i < args.length; i++)
-			System.out.println(args[i]);
+		if(args.length > 0){
+			for(int i = 0; i < args.length; i++)
+				System.out.println(args[i]);
+			
+		}
+		
+			System.out.println("No arg...");
 	}
 }
